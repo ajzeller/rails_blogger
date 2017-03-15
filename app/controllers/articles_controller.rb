1b37881674f7f5cfb6ca335c190c622d62bae609
Andrew <andrew.j.zeller@gmail.com>
2017-03-14 22:34:16 -0500

    Add tagging

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
-    flash.notice = "Article '#{@article.title}' Created!"
+    flash[:success] = "Article '#{@article.title}' Created!"
 
     redirect_to article_path(@article)
   end
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    flash.notice = "Article '#{@article.title}' Removed!"
+    flash[:success] = "Article '#{@article.title}' Removed!"
 
     redirect_to articles_path
   end
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
-    flash.notice = "Article '#{@article.title}' Updated!"
+    flash[:success] = "Article '#{@article.title}' Updated!"
 
     redirect_to article_path(@article)
   end
